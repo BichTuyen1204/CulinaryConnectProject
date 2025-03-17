75580c719cfeef65c5d107f7410919724e2500b1
Tuyen <tuyen12042003@gmail.com>
2025-03-17 18:42:45 +0700

    CSS footer done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ import { AiFillTikTok } from "react-icons/ai";
 import { IoLogoYoutube } from "react-icons/io";
 import { FaBuilding } from "react-icons/fa";
 import { SlEarphonesAlt } from "react-icons/sl";
-import { IoMail } from "react-icons/io5";
+import { IoMail, IoTime } from "react-icons/io5";
 import "../footer/Footer.css";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,16 +15,16 @@ import { FaUserTie } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="footer" id="footer">
-      <div className="footer-content">
+      <div className="footer-content mt-3">
         <div className="img-logo-footer">
           <Link to="/">
             <img src={Logo} alt="Culinary Connect Logo" />
           </Link>
         </div>
         <div className="footer-content-left">
-          <h2>
-            <strong>Access to us</strong>
-          </h2>
+          <h3>
+            <strong className="info-header">Access to us</strong>
+          </h3>
           <ul className="ul-icon-social">
             <li>
               <FaFacebookSquare className="facebook" />
@@ -39,40 +39,44 @@ const Footer = () => {
         </div>
 
         <div className="footer-content-center">
-          <h2>
-            <strong>Culinary Connect</strong>
-          </h2>
-          <ul>
+          <h3>
+            <strong className="info-header">Culinary Connect</strong>
+          </h3>
+          <ul className="content-culinary-connect">
             <Link to="/">
-              <li className="li-home d-flex">
-                <IoHome style={{ marginRight: "10px" }} />
+              <li className="li-home d-flex mb-4">
+                <IoHome className="icon-home" style={{ marginRight: "10px", marginTop: "2px" }} />
                 <p>Home</p>
               </li>
             </Link>
 
             <li className="li-aboutus d-flex">
-              <FaUserTie style={{ marginRight: "10px" }} />
+              <FaUserTie className="icon-person" style={{ marginRight: "10px" }} />
               <p>About us</p>
             </li>
           </ul>
         </div>
 
         <div className="footer-content-right">
-          <h2>
-            <strong>Contact</strong>
-          </h2>
+          <h3>
+            <strong className="info-header">Contact</strong>
+          </h3>
           <ul className="address-links">
             <li className="address1 d-flex">
-              <FaBuilding className="address" />
+              <FaBuilding className="address icon-building" />
               <p>14 Nguyen Van Cu, An Khanh, Ninh Kieu, Can Tho</p>
             </li>
             <li className="address1 d-flex">
-              <SlEarphonesAlt className="address" />
+              <SlEarphonesAlt className="address icon-phone" />
               <p>0999 999 999</p>
             </li>
             <li className="address1 d-flex">
-              <IoMail className="address" />
+              <IoMail className="address icon-email" />
               <p>CulinaryConnect@gmail.com</p>
+            </li>
+            <li className="address1 d-flex">
+              <IoTime className="address icon-time" />
+              <p>Opening Hours: 6 A.M - 9 P.M</p>
             </li>
           </ul>
         </div>
